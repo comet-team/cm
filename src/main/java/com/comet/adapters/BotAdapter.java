0d9c23ea7e7bcb8cf46d0e52f14ac97037bfe86f
Allexandere <39259989+Allexandere@users.noreply.github.com>
2022-09-18 15:44:56 +0300

    init

--- src/main/java/com/comet/adapters/BotAdapter.java
+++ src/main/java/com/comet/adapters/BotAdapter.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 @FeignClient(value = "bot-adapter",
-        url = "http://a5be-217-150-192-102.ngrok.io:8080/")
+        url = "http://e49f-217-150-192-102.ngrok.io/members")
 public interface BotAdapter {
 
     @PostMapping(value = "members")

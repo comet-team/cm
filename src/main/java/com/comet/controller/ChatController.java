565d93f05a4f3adb7255cbea0a4226f3d26994f1
Allexandere <39259989+Allexandere@users.noreply.github.com>
2022-09-18 15:00:54 +0300

    init

--- src/main/java/com/comet/controller/ChatController.java
+++ src/main/java/com/comet/controller/ChatController.java
@@ -2,6 +2,7 @@ package com.comet.controller;
 
 import com.comet.model.dto.ExternalStudentGroup;
 import com.comet.model.entity.StudentGroup;
+import com.comet.service.ChatService;
 import com.comet.service.GroupService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
@@ -16,14 +17,18 @@ import java.util.UUID;
 @RequestMapping("/chat")
 public class ChatController {
 
+    @Autowired
+    private ChatService chatService;
+
     @ApiOperation("Create chat for student group")
     @PostMapping("/student_group_chat")
     public void createStudentGroupChat(@PathVariable(value = "studentGroupId") UUID studentGroupId) {
-
+        chatService.createStudentGroupChat(studentGroupId);
     }
 
     @ApiOperation("Create chat for subject group")
     @PostMapping("/subject_group_chat")
     public void createSubjectGroupChat(@PathVariable(value = "subjectGroupId") UUID subjectGroupId) {
+        chatService.createSubjectGroupChat(subjectGroupId);
     }
 }
